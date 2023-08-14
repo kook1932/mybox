0ee37088dd177848ddfef458d25d8da2884a0f9b
kook1932 <gkswjddyd4@naver.com>
2023-08-14 16:33:25 +0900

    [develop] File Upload, Get File list API

---
+++ src/main/java/com/mybox/directory/controller/DirectoryController.java
@@ -0,0 +1,11 @@
+package com.mybox.directory.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api")
+@RestController
+public class DirectoryController {
+}
--- src/main/java/com/mybox/file/controller/FileController.java
+++ src/main/java/com/mybox/file/controller/FileController.java
@@ -5,19 +5,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -28,43 +32,94 @@ public class FileController {
 	@Value("${file.upload-dir}")
 	private String uploadDir;
 
-	@GetMapping("/")
-	public String listUploadedFiles(Model model) throws IOException {
-		model.addAttribute("files", Files.walk(Paths.get(uploadDir))
-				.filter(path -> !path.equals(Paths.get(uploadDir)))
-				.map(path -> path.toFile().getName())
-				.collect(Collectors.toList()));
-		return "uploadForm";
+	/**
+	 * Upload Root 폴더 하위 디렉토리의 파일 추출
+	 * @return
+	 */
+	@GetMapping("/file")
+	public ResponseEntity<Map<String, Object>> getFiles(@RequestParam(defaultValue = "", required = false) String path) {
+		return ResponseEntity.ok(getUploadFiles(path));
 	}
 
-	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file,
-								   RedirectAttributes redirectAttributes) throws IOException {
-
-		log.info("file : {}", file.getOriginalFilename());
-		if (file.isEmpty()) {
-			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
-			return "redirect:/";
-		}
+	private Map<String, Object> getUploadFiles(String subPath) {
+		Map<String, Object> responseBody = new HashMap<>();
+		final String targetPath = StringUtils.cleanPath(uploadDir + subPath);
 
 		try {
-			String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-			Path targetLocation = Paths.get(uploadDir).resolve(fileName);
-			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+			List<Map<String, String>> filePaths = Files.walk(Paths.get(targetPath), 1)
+					.filter(path -> !path.equals(Paths.get(targetPath)))
+					.map(FileController::getFileInfo)
+					.collect(Collectors.toList());
 
-			redirectAttributes.addFlashAttribute("message",
-					"You successfully uploaded " + fileName + "!");
+			responseBody.put("result", "success");
+			responseBody.put("files", filePaths);
+			responseBody.put("path", targetPath);
 		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			file.getInputStream().close();
+			log.error("Not Found Files : {}", e.getMessage());
+			responseBody.put("result", "fail");
 		}
 
-		return "redirect:/";
+		return responseBody;
 	}
 
-	@GetMapping("/files/{filename:.+}")
-	@ResponseBody
+	private static Map<String, String> getFileInfo(Path path) {
+		Map<String, String> fileInfo = new HashMap<>();
+		fileInfo.put("fileName", path.toFile().getName());
+		fileInfo.put("contentType", getContentType(path));
+		return fileInfo;
+	}
+
+	private static String getContentType(Path path) {
+		String contentType = "";
+		try {
+			contentType = Files.probeContentType(path);
+			if (!StringUtils.hasText(contentType)) {
+				contentType = checkFileIsDirectory(path);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return contentType;
+	}
+
+	private static String checkFileIsDirectory(Path path) {
+		File noContentType = new File(path.toUri());
+		return noContentType.isDirectory() ? "directory" : "";
+	}
+
+	@PostMapping("/file")
+	public ResponseEntity<String> saveUploadFiles(@RequestParam List<MultipartFile> files,
+												  @RequestParam String savePath) {
+		if (files.isEmpty()) {
+			return new ResponseEntity<>("file upload Fail : Empty File", new HttpHeaders(), HttpStatus.BAD_REQUEST);
+		}
+
+		files.forEach(file -> saveMultipartFile(uploadDir + savePath, file));
+		return ResponseEntity.ok("file upload success");
+	}
+
+	private static void saveMultipartFile(String finalPath, MultipartFile file) {
+		try (InputStream is = file.getInputStream()) {
+			String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+			Path targetLocation = Paths.get(finalPath).resolve(fileName);
+
+			File targetDirectory = new File(targetLocation.toUri()).getParentFile();
+			if (!targetDirectory.exists()) {
+				if (targetDirectory.mkdirs()) {
+					log.error("Target directory created.");
+				} else {
+					log.error("Failed to create target directory.");
+				}
+			}
+
+			Files.copy(is, targetLocation, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			log.error("Cannot save file : {}", e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	@GetMapping("/file/{filename:.+}")
 	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 		try {
 			Path filePath = Paths.get(uploadDir).resolve(filename);
